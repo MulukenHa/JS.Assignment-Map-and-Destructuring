64cc43adf19e6582dc045454ac8fb1e1294bbf13
MulukenHa <mulukenanteneh670@gmail.com>
2024-12-12 09:41:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//task 1
+firstColor, secondColor, thirdColor = ['red', 'green', 'blue']
+
+
+//task2
+const person = {name: 'Alice', age: 25, city: 'Wonderland' }
+({myname, myage, mycity} = person)
+
+
+//task3
+const settings = { theme: 'dark', language: 'en' }
+({mytheme="light", mylanguage} = settings)
